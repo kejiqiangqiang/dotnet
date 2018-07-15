ffe41fe631be384cde4c93b48801a091b39daef6
caiq <974897633@qq.com>
2018-07-15 21:26:06 +0800

    Initial commit

---
+++ AOPProxy/AOPTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOPTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new UnitTest1();
+            test.TestMethod1();
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
